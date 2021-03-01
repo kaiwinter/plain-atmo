dd10233ee7d4f7f892edf286e2d0ae54cd5aec77
Kai Winter <kaiwinter@gmx.de>
2021-03-01 10:57:00 +0100

    added missing class

---
+++ app/src/main/java/com/github/kaiwinter/myatmo/chart/rest/model/Body.java
@@ -0,0 +1,8 @@
+package com.github.kaiwinter.myatmo.chart.rest.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Body {
+    public List<Measurement> measurements = new ArrayList<>();
+}
