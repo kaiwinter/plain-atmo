b3f5dddde771bacac4064d0c4d2cb208be5a21ad
Kai Winter <kaiwinter@gmx.de>
2021-05-07 21:44:44 +0200

    Added tests

--- app/src/test/java/com/github/kaiwinter/myatmo/main/MainActivityViewModelTest.java
+++ app/src/test/java/com/github/kaiwinter/myatmo/main/MainActivityViewModelTest.java
@@ -9,6 +9,7 @@ import com.github.kaiwinter.myatmo.util.SingleLiveEvent;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,15 +30,12 @@ public class MainActivityViewModelTest {
     @Rule
     public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
 
-    private MockWebServer mockWebServer = new MockWebServer();
-
     /**
      * The access token is missing when data should be loaded. The app should navigate to the LoginActivity.
      */
     @Test
-    public void switchToMainActivity() {
-        SharedPreferencesStore sharedPreferences =
-                when(mock(SharedPreferencesStore.class).getAccessToken()).thenReturn("").getMock();
+    public void getdata_notLoggedIn_switchToMainActivity() {
+        SharedPreferencesStore sharedPreferences = when(mock(SharedPreferencesStore.class).getAccessToken()).thenReturn("").getMock();
         MainActivityViewModel viewModel = new MainActivityViewModel(null, sharedPreferences, null, null);
 
         viewModel.navigateToLoginActivity = mock(SingleLiveEvent.class);
@@ -46,37 +44,65 @@ public class MainActivityViewModelTest {
         verify(viewModel.navigateToLoginActivity).call();
     }
 
+    /**
+     * Receives a valid JSON from the mock server and check if the viewmodel is filled accordingly.
+     */
     @Test
-    public void refreshAccessToken() throws IOException {
-        InputStream resourceAsStream = MainActivityViewModelTest.class.getResourceAsStream("/stationsdata_200.txt");
-        Buffer buffer = new Buffer().readFrom(resourceAsStream);
-        mockWebServer.enqueue(new MockResponse().setBody(buffer));
+    public void getdata_valid() throws IOException {
+
+        runWithMockWebServer("/stationsdata_200.json", 200, () -> {
+
+            SharedPreferencesStore sharedPreferences = when(mock(SharedPreferencesStore.class).getAccessToken()).thenReturn("ABC").getMock();
+            AccessTokenManager tokenManager = mock(AccessTokenManager.class);
+            StationsDataService service = ServiceGenerator.createService(StationsDataService.class);
+
+            MainActivityViewModel viewModel = new MainActivityViewModel(null, sharedPreferences, tokenManager, service);
+            viewModel.getdata();
+
+            await().atMost(10, TimeUnit.SECONDS).until(() -> viewModel.outdoorModule.getValue() != null);
+
+            ModuleVO outdoor = viewModel.outdoorModule.getValue();
+            assertEquals("Schlafzimmer", outdoor.moduleName);
+            assertEquals(ModuleVO.ModuleType.OUTDOOR, outdoor.moduleType);
+            assertEquals(1615837300, outdoor.beginTime.longValue());
+            assertEquals(56, outdoor.humidity.longValue());
+            assertEquals(20.3, outdoor.temperature, 0);
+            assertEquals("B_ID", outdoor.id);
+
+            ModuleVO indoor = viewModel.indoorModule.getValue();
+            assertEquals("Wohnzimmer", indoor.moduleName);
+            assertEquals(ModuleVO.ModuleType.INDOOR, indoor.moduleType);
+            assertEquals(1615837309, indoor.beginTime.longValue());
+            assertEquals(1087, indoor.co2.longValue());
+            assertEquals(49, indoor.humidity.longValue());
+            assertEquals(21.5, indoor.temperature, 0);
+        });
+    }
+
+    @Test
+    public void getdata_401() throws IOException {
+        runWithMockWebServer("/401_2.json", 401, () -> {
+            SharedPreferencesStore sharedPreferences = when(mock(SharedPreferencesStore.class).getAccessToken()).thenReturn("ABC").getMock();
+            AccessTokenManager tokenManager = mock(AccessTokenManager.class);
+            StationsDataService service = ServiceGenerator.createService(StationsDataService.class);
+
+            MainActivityViewModel viewModel = new MainActivityViewModel(null, sharedPreferences, tokenManager, service);
+
+            viewModel.navigateToRelogin = mock(SingleLiveEvent.class);
+            viewModel.getdata();
+
+            await().untilAsserted(() -> viewModel.navigateToRelogin.postValue(ArgumentMatchers.eq("401: Invalid token missing (2)")));
+        });
+    }
+
+    private void runWithMockWebServer(String file, int httpCode, Runnable runnable) throws IOException {
+        InputStream inputStream = MainActivityViewModelTest.class.getResourceAsStream(file);
+        Buffer buffer = new Buffer().readFrom(inputStream);
+
+        MockWebServer mockWebServer = new MockWebServer();
+        mockWebServer.enqueue(new MockResponse().setBody(buffer).setResponseCode(httpCode));
         mockWebServer.start(8080);
-        SharedPreferencesStore sharedPreferences = mock(SharedPreferencesStore.class);
-
-        when(sharedPreferences.getAccessToken()).thenReturn("ABC");
-        AccessTokenManager tokenManager = mock(AccessTokenManager.class);
-        StationsDataService service = ServiceGenerator.createService(StationsDataService.class);
-
-        MainActivityViewModel viewModel = new MainActivityViewModel(null, sharedPreferences, tokenManager, service);
-        viewModel.getdata();
-
-        await().atMost(20, TimeUnit.SECONDS).until(() -> viewModel.outdoorModule.getValue() != null);
-
-        ModuleVO outdoor = viewModel.outdoorModule.getValue();
-        assertEquals("Schlafzimmer", outdoor.moduleName);
-        assertEquals(ModuleVO.ModuleType.OUTDOOR, outdoor.moduleType);
-        assertEquals(1615837300, outdoor.beginTime.longValue());
-        assertEquals(56, outdoor.humidity.longValue());
-        assertEquals(20.3, outdoor.temperature, 0);
-        assertEquals("B_ID", outdoor.id);
-
-        ModuleVO indoor = viewModel.indoorModule.getValue();
-        assertEquals("Wohnzimmer", indoor.moduleName);
-        assertEquals(ModuleVO.ModuleType.INDOOR, indoor.moduleType);
-        assertEquals(1615837309, indoor.beginTime.longValue());
-        assertEquals(1087, indoor.co2.longValue());
-        assertEquals(49, indoor.humidity.longValue());
-        assertEquals(21.5, indoor.temperature, 0);
+        runnable.run();
+        mockWebServer.close();
     }
 }
--- app/src/test/java/com/github/kaiwinter/myatmo/main/ServiceGenerator.java
+++ app/src/test/java/com/github/kaiwinter/myatmo/main/ServiceGenerator.java
@@ -35,36 +35,4 @@ public class ServiceGenerator {
         return retrofit.create(serviceClass);
     }
 
-    public static <S> S createService(Class<S> serviceClass, final String authToken) {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor)
-                .addInterceptor(chain -> {
-                    Request newRequest = chain.request().newBuilder()
-                            .addHeader("Authorization", "Bearer " + authToken)
-                            .build();
-                    return chain.proceed(newRequest);
-                })
-                .build();
-
-        BodyTypeAdapter myAdapter = new BodyTypeAdapter();
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .registerTypeAdapter(Body.class, myAdapter)
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        return retrofit.create(serviceClass);
-    }
-
-    public static APIError parseError(retrofit2.Response<?> response) {
-        Gson gson = new Gson();
-        return gson.fromJson(response.errorBody().charStream(), APIError.class);
-    }
-
 }
